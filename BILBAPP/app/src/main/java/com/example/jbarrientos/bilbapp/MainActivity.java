299340355a274270585e3871506fae2461d8d670
Josu Barrientos <josu_barrientos@hotmail.com>
2016-12-29 20:35:42 +0100

    added greeting activity

---
+++ BILBAPP/app/src/main/java/com/example/jbarrientos/bilbapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.jbarrientos.bilbapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.*;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int TIME_OUT = 4000; //Time to launch the another activity
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, LanguageSelection.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);*/
+    }
+}
