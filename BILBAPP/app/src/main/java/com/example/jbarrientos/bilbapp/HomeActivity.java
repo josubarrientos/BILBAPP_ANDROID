14cfaab86c29aa5b2e2fa3a532a2d9b7effc74bd
Josu Barrientos <josu_barrientos@hotmail.com>
2017-01-04 18:40:52 +0100

    added help dialogs and home activity

--- BILBAPP/app/src/main/java/com/example/jbarrientos/bilbapp/HomeActivity.java
+++ BILBAPP/app/src/main/java/com/example/jbarrientos/bilbapp/HomeActivity.java
@@ -1,7 +1,12 @@
 package com.example.jbarrientos.bilbapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.content.DialogInterface;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,8 +14,25 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        new AlertDialog.Builder(this).setTitle(R.string.instrucciones1).setMessage(R.string.texto_instrucciones1).setNeutralButton(R.string.accept_bottom, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                secondDialog();
+            }
+        }).show();
+
+
     }
 
+    public void secondDialog() {
 
+        AlertDialog.Builder alertadd = new AlertDialog.Builder(this).setTitle(R.string.instrucciones1);
+        LayoutInflater factory = LayoutInflater.from(this);
+        final View view = factory.inflate(R.layout.help_layout, null);
+        alertadd.setView(view);
+        alertadd.setNeutralButton(R.string.accept_bottom, null);
+        alertadd.show();
+
+    }
 
 }
