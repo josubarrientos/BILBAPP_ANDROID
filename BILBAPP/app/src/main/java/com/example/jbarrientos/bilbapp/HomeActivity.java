a3f645bf0d379b772b919a86f25d369f67fabb38
Josu Barrientos <josu_barrientos@hotmail.com>
2017-01-04 19:21:35 +0100

    added home activity methods

--- BILBAPP/app/src/main/java/com/example/jbarrientos/bilbapp/HomeActivity.java
+++ BILBAPP/app/src/main/java/com/example/jbarrientos/bilbapp/HomeActivity.java
@@ -1,12 +1,16 @@
 package com.example.jbarrientos.bilbapp;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.content.DialogInterface;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -21,7 +25,6 @@ public class HomeActivity extends AppCompatActivity {
             }
         }).show();
 
-
     }
 
     public void secondDialog() {
@@ -32,6 +35,48 @@ public class HomeActivity extends AppCompatActivity {
         alertadd.setView(view);
         alertadd.setNeutralButton("OK",null);
         alertadd.show();
+
+    }
+
+    public void selectFiesta(View v){
+
+        Toast.makeText(getApplicationContext(), "Fiesta!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void selectCompra(View v){
+
+        Toast.makeText(getApplicationContext(), "Compras!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void selectRestaurante(View v){
+
+        Toast.makeText(getApplicationContext(), "Restaurantes!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void selectHotel(View v){
+
+        Toast.makeText(getApplicationContext(), "Hoteles!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void selectDeporte(View v){
+
+        Toast.makeText(getApplicationContext(), "Deportes!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void selectMonumento(View v){
+
+        Toast.makeText(getApplicationContext(), "Monumentos!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void selectTransporte(View v){
+
+        Toast.makeText(getApplicationContext(), "Transportes!", Toast.LENGTH_SHORT).show();
 
     }
 
