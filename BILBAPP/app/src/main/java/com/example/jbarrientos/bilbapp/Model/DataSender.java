7ee39b27867d3e5b7583cc1803db16cf4e93ca81
Josu Barrientos <josu_barrientos@hotmail.com>
2017-01-05 19:49:57 +0100

    updated dialog functionalities

--- BILBAPP/app/src/main/java/com/example/jbarrientos/bilbapp/Model/DataSender.java
+++ BILBAPP/app/src/main/java/com/example/jbarrientos/bilbapp/Model/DataSender.java
@@ -2,22 +2,13 @@ package com.example.jbarrientos.bilbapp.Model;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.jbarrientos.bilbapp.R;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
 
 /**
  * Created by jbarrientos on 5/01/17.
@@ -38,11 +29,8 @@ public class DataSender {
         LayoutInflater factory = LayoutInflater.from(ctx);
         final View f = factory.inflate(R.layout.experience_insert, null);
 
-        loginDialog.setTitle("Please enter your name and email");
+        loginDialog.setTitle("Please enter your name and you experience");
         loginDialog.setView(f);
-
-
-        //loginDialog.show();
 
         Button submit = (Button)f.findViewById(R.id.submit);
 
@@ -53,11 +41,11 @@ public class DataSender {
             @Override
             public void onClick(View v) {
 
-                EditText full_name = (EditText)f.findViewById(R.id.follower_name);
-                EditText email_address = (EditText)f.findViewById(R.id.follower_email);
+                EditText name = (EditText)f.findViewById(R.id.user_name);
+                EditText opinion = (EditText)f.findViewById(R.id.user_opinion);
 
-                String follower_name = full_name.getText().toString();
-                String follower_email = email_address.getText().toString();
+                String user_name = name.getText().toString();
+                String user_opinion = opinion.getText().toString();
 
                 dialog.dismiss();
 
