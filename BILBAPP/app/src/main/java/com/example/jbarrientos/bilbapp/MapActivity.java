ac1f24cc2243c5a20a3ab2271d29e216130419c9
Josu Barrientos <josu_barrientos@hotmail.com>
2017-01-05 17:50:47 +0100

    now MapActivity get data for its adapter through Model

--- BILBAPP/app/src/main/java/com/example/jbarrientos/bilbapp/MapActivity.java
+++ BILBAPP/app/src/main/java/com/example/jbarrientos/bilbapp/MapActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.example.jbarrientos.bilbapp.Model.DataPopulator;
 import com.example.jbarrientos.bilbapp.Model.Sitios;
 
 import java.util.ArrayList;
@@ -17,19 +18,39 @@ public class MapActivity extends AppCompatActivity {
 
         setTitle(R.string.help_icon_2);
 
+        String sitioType = getIntent().getStringExtra("extra_text");
+
         ListView lista = (ListView) findViewById(R.id.info_list);
 
-        ArrayList<Sitios> versiones =
-                new ArrayList<Sitios>();
+        DataPopulator recogedor = new DataPopulator();
 
-        versiones.add(
-                new Sitios("El Corte Ingles", 3,"https://www.google.es/maps/place/El+Corte+Ingl%C3%A9s,+Bilbo,+Bizkaia/@43.2611946,-2.9312729,17z/data=!3m1!4b1!4m5!3m4!1s0xd4e4fd0362d1f5d:0x30af64dfd131ed33!8m2!3d43.2611424!4d-2.9289884"));
-        versiones.add(
-                new Sitios("Zara", 5,"https://www.google.es/maps/place/Zara/@43.2611946,-2.9312729,17z/data=!4m8!1m2!2m1!1szara+bilbao!3m4!1s0xd4e4fd77461cd9f:0x7bb79b49f6ad96d!8m2!3d43.2618447!4d-2.9345436"));
-        versiones.add(
-                new Sitios("Fnac", 4,"https://www.google.es/maps/place/FNAC+BILBAO/@43.2613269,-2.9322121,17z/data=!3m1!4b1!4m5!3m4!1s0xd4e4fd057f5e725:0x1149334bc16a475a!8m2!3d43.2613269!4d-2.9300234"));
-        versiones.add(
-                new Sitios("Pull and Bear", 2,"https://www.google.es/maps/place/Pull%26Bear/@43.2623615,-2.9335082,17z/data=!3m1!4b1!4m5!3m4!1s0xd4e4fd0b0f00693:0x9c1062f7b6902052!8m2!3d43.2623615!4d-2.9313195"));
+        ArrayList<Sitios> versiones = new ArrayList<Sitios>();
+
+        switch (sitioType) {
+
+            case "fiesta":
+                versiones = recogedor.cargaInfoSitiosFiesta(this);
+                break;
+            case "compras":
+                versiones = recogedor.cargaInfoSitiosCompras(this);
+                break;
+            case "restaurantes":
+                versiones = recogedor.cargaInfoSitiosRestaurantes(this);
+                break;
+            case "hotel":
+                versiones = recogedor.cargaInfoSitiosAlojamiento(this);
+                break;
+            case "deporte":
+                versiones = recogedor.cargaInfoSitiosDeportes(this);
+                break;
+            case "monumentos":
+                versiones = recogedor.cargaInfoSitiosMonumetnos(this);
+                break;
+            case "transporte":
+                versiones = recogedor.cargaInfoSitiosTransportes(this);
+            default:
+                break;
+        }
 
         NameOfSitiosAdapter adaptador = new NameOfSitiosAdapter(this, versiones);
         lista.setAdapter(adaptador);
